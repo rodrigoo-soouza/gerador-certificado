1041e2305d04f1e7f62552b72fea5c7ed56f1838
rodrigoo-soouza <digosouzafonseca@gmail.com>
2025-11-04 15:40:51 -0300

    Consertando Form

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from "../../_components/secondary-button/secondary-button";
 import { PrimaryButton } from "../../_components/primary-button/primary-button";
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButton, PrimaryButton],
+  imports: [SecondaryButton, PrimaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css',
 })
 export class CertificadoFormComponent {
+  nome: string = '';
+  atividade: string = '';
+  listaAtividades: string [] = ['Angular', 'React', 'C#'];
 
+  campoInvalido(control: NgModel){
+    return control.invalid && control.touched
+  }
 }
